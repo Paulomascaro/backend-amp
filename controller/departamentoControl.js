ff35f13e885315245f5a6fc1bb951e91d2ca7b9c
mgmaia13 <maia.m@aluno.ifsp.edu.br>
2023-04-18 16:09:53 -0300

    Tratamento de not null em todos os post e put Tratamento de erro em todas as requisições

--- controller/departamentoControl.js
+++ controller/departamentoControl.js
@@ -3,61 +3,88 @@ const Departamento = require('../modelo/departamento');
 
 // cadastrando departamento
 router.post('/adicionar', async (req, res) => {
-    await Departamento.sync();
-    console.log(`inserindo departamento ${req.body.nome} no banco`);
-    const departamento = await Departamento.create({
-        nome: req.body.nome,
-        sala: req.body.sala,
-        telefone: req.body.telefone
-    })
-    console.log(req.body)
-    res.json(departamento);
+    try {
+        if (req.body.nome != null && req.body.sala != null && req.body.telefone != null) {
+            await Departamento.sync();
+            console.log(`inserindo departamento ${req.body.nome} no banco`);
+            const departamento = await Departamento.create({
+                nome: req.body.nome,
+                sala: req.body.sala,
+                telefone: req.body.telefone
+            })
+            console.log(req.body)
+            res.json(departamento);
+        } else {
+            res.status(422).json({Erro: "Parametros faltando!"})
+        }
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // listando todos os departamentos
 router.get('/listartodos', async (req, res) => {
-    console.log("Muito sexo");
-    await Departamento.sync();
-    console.log(`listando todos departamentos ${req.body.nome} no banco`);
-    const departamentos = await Departamento.findAll();
+    try {
+        await Departamento.sync();
+        console.log(`listando todos departamentos ${req.body.nome} no banco`);
+        const departamentos = await Departamento.findAll();
 
-    res.send(JSON.stringify(departamentos, null, 2));
+        res.send(JSON.stringify(departamentos, null, 2));
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // listar unico departamento
 router.get('/listardepartamento/:nome', async (req, res) => {
-    await Departamento.sync();
-    console.log(`listando departamento ${req.params.nome}`);
-    const departamento = await Departamento.findOne({
-        where: { nome: req.params.nome }
-    });
+    try {
+        await Departamento.sync();
+        console.log(`listando departamento ${req.params.nome}`);
+        const departamento = await Departamento.findOne({
+            where: {nome: req.params.nome}
+        });
 
-    res.json(departamento);
+        res.json(departamento);
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // deletando departamento
 router.delete('/excluir/:nome', async (req, res) => {
-    await Departamento.sync();
-    const departamento = await Departamento.destroy({
-        where:{nome: req.params.nome}
-    });
+    try {
+        await Departamento.sync();
+        const departamento = await Departamento.destroy({
+            where: {nome: req.params.nome}
+        });
 
-    res.send(`departamento ${req.params.nome} excluido!`);
+        res.send(`departamento ${req.params.nome} excluido!`);
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // editar departamento
 router.put('/editar/:nome', async (req, res) => {
-    await Departamento.sync();
+    try {
+        if (req.body.nome != null && req.body.sala != null && req.body.telefone != null) {
+            await Departamento.sync();
 
-    const departamento = await Departamento.update({
-        nome: req.body.nome,
-        sala: req.body.sala,
-        telefone: req.body.telefone
-    }, {
-        where:{nome: req.params.nome}
-    });
+            const departamento = await Departamento.update({
+                nome: req.body.nome,
+                sala: req.body.sala,
+                telefone: req.body.telefone
+            }, {
+                where: {nome: req.params.nome}
+            });
 
-    res.send(`departamento ${req.params.nome} editado!`);
+            res.send(`departamento ${req.params.nome} editado!`);
+        } else {
+            res.status(422).json({Erro: "Parametros faltando!"})
+        }
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 
--- controller/usuarioControl.js
+++ controller/usuarioControl.js
@@ -3,60 +3,88 @@ const Usuario = require('../modelo/usuario');
 
 // cadastrando usuario
 router.post('/adicionar', async (req, res) => {
-    await Usuario.sync();
-    console.log(`inserindo usuario ${req.body.nome} no banco`);
-    const usuario = await Usuario.create({
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha
-    })
-    console.log(req.body)
-    res.json(usuario);
+    try {
+        await Usuario.sync();
+        if(req.body.nome != null && req.body.senha != null && req.body.email != null) {
+            console.log(`inserindo usuario ${req.body.nome} no banco`);
+            const usuario = await Usuario.create({
+                nome: req.body.nome,
+                email: req.body.email,
+                senha: req.body.senha
+            })
+            console.log(req.body)
+            res.json(usuario)
+        } else {
+            res.status(422).json({Erro: "Parametros faltando"})
+        }
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // listando todos os usuarios
 router.get('/listartodos', async (req, res) => {
-    await Usuario.sync();
-    console.log(`listando todos usuarios ${req.body.nome} no banco`);
-    const usuarios = await Usuario.findAll();
+    try {
+        await Usuario.sync();
+        console.log(`listando todos usuarios ${req.body.nome} no banco`);
+        const usuarios = await Usuario.findAll();
 
-    res.send(JSON.stringify(usuarios, null, 2));
+        res.send(JSON.stringify(usuarios, null, 2));
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // listar unico usuario
 router.get('/listarusuario/:email', async (req, res) => {
-    await Usuario.sync();
-    console.log(`listando usuario ${req.body.email}`);
-    const usuario = await Usuario.findOne({ 
-        where: { email: req.params.email }
-    });
-    
-    res.json(usuario);
+    try {
+        await Usuario.sync();
+        console.log(`listando usuario ${req.body.email}`);
+        const usuario = await Usuario.findOne({
+            where: {email: req.params.email}
+        });
+
+        res.json(usuario);
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // deletando usuario
 router.delete('/excluir/:email', async (req, res) => {
-    await Usuario.sync();
-    const usuario = await Usuario.destroy({
-        where:{email: req.params.email}
-    });
-    
-    res.send(`usuario ${req.params.email} excluido!`);
+    try {
+        await Usuario.sync();
+        const usuario = await Usuario.destroy({
+            where: {email: req.params.email}
+        });
+
+        res.send(`usuario ${req.params.email} excluido!`);
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 // editar usuario
 router.put('/editar/:email', async (req, res) => {
-    await Usuario.sync();
+    try {
+        if (req.body.nome != null && req.body.senha != null && req.body.email != null) {
+            await Usuario.sync();
 
-    const usuario = await Usuario.update({
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha
-    }, {
-        where:{email: req.params.email}
-    });
-    
-    res.send(`usuario ${req.params.email} editado!`);
+            const usuario = await Usuario.update({
+                nome: req.body.nome,
+                email: req.body.email,
+                senha: req.body.senha
+            }, {
+                where: {email: req.params.email}
+            });
+
+            res.send(`usuario ${req.params.email} editado!`);
+        } else {
+            res.status(422).json({Erro: "Parametros faltando!"})
+        }
+    } catch (e) {
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
 });
 
 
